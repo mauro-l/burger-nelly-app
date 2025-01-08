25ad06d8073efff3d8d45204068fd80126f281bc
Nelida <neessu2023@gmail.com>
2025-01-08 14:31:27 -0300

    Guardando cambios locales en App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ function App() {
   return (
     <div className="flex justify-center items-center w-full">
       <h1 className="text-5xl font-bold text-teal-500">
-        Hola nelly desde TailwindCss
+        Hola nelly desde TailwindCss 
       </h1>
     </div>
   );
