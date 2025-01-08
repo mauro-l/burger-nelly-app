bc60ae4417efb9f4a6e5a4256f15ebbc4f0cf526
Mauro Laime <maurol.dev@gmail.com>
2025-01-08 12:11:04 -0300

    se crea la estructura del mini proyecto

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="flex justify-center items-center w-full">
+      <h1 className="text-5xl font-bold text-teal-500">
+        Hola nelly desde TailwindCss
+      </h1>
+    </div>
+  );
+}
+
+export default App;
+
